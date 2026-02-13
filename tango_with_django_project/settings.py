0a6992aca995d9f5e5a1bb6810acec0f6db8471f
Phoevos Athineos <3015850A@student.gla.ac.uk>
2026-02-13 01:57:24 +0000

    Task 9 commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     }
 }
 
+# Password hashers
+
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+'django.contrib.auth.hashers.BCryptPasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -127,3 +135,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+# Login stuff
+
+LOGIN_URL = 'rango:login'
